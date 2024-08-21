c1bb6ad65461abfd4156da6fa7d6b2cbe59591ba
wyattgable69 <85705082+wyattgable69@users.noreply.github.com>
2024-08-21 16:53:38 -0400

    BombHero Fix

---
+++ BombHero/offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1724271882,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/btn_numbers-sheet0.png",
+		"images/btn_numbers-sheet1.png",
+		"images/btn_numbers-sheet2.png",
+		"images/btn_confirmcancel-sheet0.png",
+		"images/btn_confirmcancel-sheet1.png",
+		"images/sprite-sheet0.png",
+		"images/btn_start-sheet0.png",
+		"images/bomb-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"media/instructions.ogg",
+		"media/numbers.ogg",
+		"media/youwin.ogg",
+		"media/item_get.ogg",
+		"media/nuclear explosion sound effect.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
